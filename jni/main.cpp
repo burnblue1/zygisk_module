461abda46157dd2b26a50c3a3bf3779e1db54da7
burnblue1 <burnblue@qq.com>
2026-07-25 12:45:53 +0800

    Update main.cpp

--- jni/main.cpp
+++ jni/main.cpp
@@ -9,7 +9,7 @@
 #include <mutex>
 #include <thread>
 #include <chrono>
-
+#include "bridge.h"
 #define TAG "GameHelper"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
