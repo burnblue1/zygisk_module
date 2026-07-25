d8b5fbecb1eb41ef5333438b519666a2912e4c37
burnblue1 <burnblue@qq.com>
2026-07-25 12:40:25 +0800

    Update bridge.cpp

--- jni/bridge.cpp
+++ jni/bridge.cpp
@@ -7,7 +7,7 @@
 #include <cstring>
 #include <sstream>
 #include <algorithm>
-
+#include <android/log.h>
 #define TAG "Bridge"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
